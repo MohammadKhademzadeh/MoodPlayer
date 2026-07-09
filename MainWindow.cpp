4622b46d46f22be7d41d3a93a9166035f0ae6f13
Mohammad Khademzadeh <291937255+MohammadKhademzadeh@users.noreply.github.com>
2026-07-10 03:29:49 +0330

    Add music folder and player controls to main UI

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,11 +1,13 @@
 #include "MainWindow.h"
 #include <QVBoxLayout>
+#include <QFileDialog>
+#include <QPushButton>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), isMonitoring(false)
 {
     setWindowTitle("MoodPlayer - Smart Music");
-    resize(450, 250);
+    resize(450, 300);
 
     QWidget *central = new QWidget(this);
     QVBoxLayout *layout = new QVBoxLayout(central);
@@ -13,16 +15,32 @@ MainWindow::MainWindow(QWidget *parent)
     statusLabel = new QLabel("Status: Idle", this);
     cpuLabel = new QLabel("CPU: 0%", this);
     startStopButton = new QPushButton("Start Monitoring", this);
+    auto folderButton = new QPushButton("Choose Music Folder", this);
+    auto nextButton = new QPushButton("Next Track", this);
 
     layout->addWidget(statusLabel);
     layout->addWidget(cpuLabel);
     layout->addWidget(startStopButton);
+    layout->addWidget(folderButton);
+    layout->addWidget(nextButton);
 
     setCentralWidget(central);
 
     timer = new QTimer(this);
+
     connect(timer, &QTimer::timeout, this, &MainWindow::updateSystemStatus);
     connect(startStopButton, &QPushButton::clicked, this, &MainWindow::toggleMonitoring);
+    connect(folderButton, &QPushButton::clicked, this, [this]() {
+        QString folder = QFileDialog::getExistingDirectory(this, "Music Folder");
+        if (!folder.isEmpty()) {
+            music.setMusicFolder(folder);
+            statusLabel->setText("Music folder loaded");
+        }
+    });
+    connect(nextButton, &QPushButton::clicked, this, [this]() {
+        music.playNext();
+        statusLabel->setText("Track: " + music.currentTrack());
+    });
 }
 
 MainWindow::~MainWindow() {}
@@ -44,7 +62,6 @@ void MainWindow::updateSystemStatus()
 {
     double cpu = monitor.cpuUsage();
     QString app = monitor.activeWindow();
-
     auto mode = MoodEngine::detect(app, cpu);
 
     cpuLabel->setText(QString("CPU: %1%").arg(cpu, 0, 'f', 1));
