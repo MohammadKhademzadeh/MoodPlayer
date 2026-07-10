0612e40eb6a9fbbfa6efddd6400813d397f78bf4
Mohammad Khademzadeh <291937255+MohammadKhademzadeh@users.noreply.github.com>
2026-07-10 03:58:54 +0330

    Expand settings dialog controls

--- src/ui/SettingsDialog.h
+++ src/ui/SettingsDialog.h
@@ -1,10 +1,19 @@
 #pragma once
 
 #include <QDialog>
+#include <QSlider>
+#include <QCheckBox>
+#include <QLineEdit>
+#include <QPushButton>
 
 class SettingsDialog : public QDialog {
     Q_OBJECT
 
 public:
     explicit SettingsDialog(QWidget* parent = nullptr);
+
+private:
+    QLineEdit* musicPath;
+    QSlider* volumeSlider;
+    QCheckBox* autoPlay;
 };
