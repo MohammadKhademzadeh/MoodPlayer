ebeca4816aa2ce62e0dae25743e2570f76fc58de
Mohammad Khademzadeh <291937255+MohammadKhademzadeh@users.noreply.github.com>
2026-07-10 03:58:59 +0330

    Implement settings dialog UI

--- src/ui/SettingsDialog.cpp
+++ src/ui/SettingsDialog.cpp
@@ -6,5 +6,19 @@ SettingsDialog::SettingsDialog(QWidget* parent) : QDialog(parent) {
     setWindowTitle("MoodPlayer Settings");
 
     auto* layout = new QVBoxLayout(this);
-    layout->addWidget(new QLabel("Music folder and player settings will appear here."));
+
+    musicPath = new QLineEdit(this);
+    musicPath->setPlaceholderText("Default music folder");
+
+    volumeSlider = new QSlider(Qt::Horizontal, this);
+    volumeSlider->setRange(0, 100);
+    volumeSlider->setValue(70);
+
+    autoPlay = new QCheckBox("Auto play on startup", this);
+
+    layout->addWidget(new QLabel("Music folder"));
+    layout->addWidget(musicPath);
+    layout->addWidget(new QLabel("Volume"));
+    layout->addWidget(volumeSlider);
+    layout->addWidget(autoPlay);
 }
