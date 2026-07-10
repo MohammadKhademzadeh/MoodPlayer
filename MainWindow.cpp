37cc88cb643ebf3962fd26e847788b58d030551f
Mohammad Khademzadeh <291937255+MohammadKhademzadeh@users.noreply.github.com>
2026-07-10 03:52:48 +0330

    Add interactive playlist selection UI

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -6,7 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), isMonitoring(false)
 {
     setWindowTitle("MoodPlayer - Smart Music");
-    resize(450, 360);
+    resize(500, 500);
 
     QWidget *central = new QWidget(this);
     QVBoxLayout *layout = new QVBoxLayout(central);
@@ -19,9 +19,11 @@ MainWindow::MainWindow(QWidget *parent)
     playButton = new QPushButton("Play", this);
     pauseButton = new QPushButton("Pause", this);
     stopButton = new QPushButton("Stop", this);
+    playlistWidget = new QListWidget(this);
 
     layout->addWidget(statusLabel);
     layout->addWidget(cpuLabel);
+    layout->addWidget(playlistWidget);
     layout->addWidget(startStopButton);
     layout->addWidget(folderButton);
     layout->addWidget(nextButton);
@@ -39,6 +41,7 @@ MainWindow::MainWindow(QWidget *parent)
     connect(playButton, &QPushButton::clicked, this, &MainWindow::playMusic);
     connect(pauseButton, &QPushButton::clicked, this, &MainWindow::pauseMusic);
     connect(stopButton, &QPushButton::clicked, this, &MainWindow::stopMusic);
+    connect(playlistWidget, &QListWidget::itemDoubleClicked, this, &MainWindow::playSelectedTrack);
     connect(nextButton, &QPushButton::clicked, this, [this]() {
         music.playNext();
         statusLabel->setText("Track: " + music.currentTrack());
@@ -57,18 +60,27 @@ void MainWindow::chooseMusicFolder()
     if (!folder.isEmpty()) {
         music.setMusicFolder(folder);
         settings.setMusicDirectory(folder);
+        playlistWidget->clear();
+        playlistWidget->addItems(music.playlist());
         statusLabel->setText("Music folder loaded");
     }
+}
+
+void MainWindow::playSelectedTrack(QListWidgetItem *item)
+{
+    int index = playlistWidget->row(item);
+    music.playTrack(index);
+    statusLabel->setText("Playing: " + music.currentTrack());
 }
 
 void MainWindow::playMusic()
 {
     music.play();
-    statusLabel->setText("Playing: " + music.currentTrack());
 }
 
 void MainWindow::pauseMusic()
 {
+    music.pause();
     statusLabel->setText("Paused");
 }
 
